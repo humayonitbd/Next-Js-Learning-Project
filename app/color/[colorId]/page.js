3cf691999557628d0a67206ef72d76ef3849f19e
humayonitbd <humayonforid44@gmail.com>
2024-02-22 22:51:13 +0600

    new feture add

--- app/color/[colorId]/page.js
+++ app/color/[colorId]/page.js
@@ -5,7 +5,10 @@ export default function colorPage({params}) {
     console.log(colorId)
   return (
     <div>
-        <div><h1>id:{colorId}</h1></div>
+        <div>
+          <h1>id:{colorId}</h1>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis vitae doloremque amet ducimus labore. Reprehenderit quod cupiditate temporibus voluptates quasi.</p>
         </div>
+    </div>
   )
 }
