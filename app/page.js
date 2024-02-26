50c2bda518a5f2a08a72206318ca851124479b2a
humayonitbd <humayonforid44@gmail.com>
2024-02-26 20:52:58 +0600

    button impletemt

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
+"use client";
 import Link from "next/link";
-import { useRouter } from "next/router";
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
   const router = useRouter();
-  const handlerbtn = ()=>{
-        router.push("/color")
-  }
+
+  const handlerbtn = () => {
+    router.push("/color");
+  };
  
   return (
     <main>
