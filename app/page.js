c254dc3423846d783b466a8872bcaaba190e0f17
humayonitbd <humayonforid44@gmail.com>
2024-02-27 18:03:00 +0600

    code customize

--- app/page.js
+++ app/page.js
@@ -1,23 +1,17 @@
-"use client";
-import Link from "next/link";
-import { useRouter } from 'next/navigation'
+// "use client";
+
 
 export default function Home() {
-  const router = useRouter();
+ 
 
-  const handlerbtn = () => {
-    router.push("/color");
-  };
+  
  
   return (
     <main>
       <div className="mt-10">
         <h1>Md Humayon Forid</h1>
         <br />
-        <Link href="/color"><button className="ml-10 btn bg-green-500 px-5 py-2 text-white text-lg">color</button></Link>
-      </div>
-      <div>
-        <button onClick={handlerbtn}>Onclick btn</button>
+        
       </div>
     </main>
   );
