cb891e28be88249ad8cbc417bfb01d629c6bd9ed
humayonitbd <humayonforid44@gmail.com>
2024-02-28 19:25:45 +0600

    admin page

--- app/colorbox/layout.jsx
+++ app/colorbox/layout.jsx
@@ -1,10 +1,12 @@
 
 export default function ColorboxLayout({children, left, right}) {
+    const sliders = true;
   return (
     <section>
       {children}
       {left}
       {right}
+      {/* {sliders ? left : right} */}
     </section>
   );
 }
